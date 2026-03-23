8135389c85cd1027dc4bc8ff7fa970df30df7cab
moosa456 <mohdmusaddik44@gmail.com>
2026-03-23 11:47:36 +0530

    latest updates

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
+  baseURL: "http://localhost:5000/api",
 });
+
+// Employee APIs
+export const getEmployees = () => API.get("/employees");
+export const createEmployee = (data) => API.post("/employees", data);
+export const updateEmployee = (id, data) => API.put(`/employees/${id}`, data);
+export const deleteEmployee = (id) => API.delete(`/employees/${id}`);
+
+// Vacation APIs
+export const getVacations = () => API.get("/vacations");
+export const createVacation = (data) => API.post("/vacations", data);
+export const updateVacation = (id, data) => API.put(`/vacations/${id}`, data);
+export const deleteVacation = (id) => API.delete(`/vacations/${id}`);
+
 
 export default API;
